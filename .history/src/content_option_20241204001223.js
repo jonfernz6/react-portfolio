87829ae9482a17ddaf8d96171dd80ac21859bd0f
cloud303-jfernandez <jfernandez@cloud303.io>
2024-12-04 00:44:56 -0800

    installing amplify

---
+++ .history/src/content_option_20241204001223.js
@@ -0,0 +1,166 @@
+const logotext = "home";
+const meta = {
+    title: "Jonathan Fernandez - AWS Evangelist",
+    description: "I’m Jonathan Fernandez - Technologist Extraordinare - a Malaysian living the American Dream in Portland, OR",
+};
+
+const introdata = {
+    title: "I’m Jonathan Fernandez",
+    animated: {
+        first: "I love solving problems",
+        second: "I geek out on automation",
+        third: "I get a kick ouf of emerging tech",
+    },
+    description: "A cloud enthusiast, tech whisperer, and your go-to guy for making impossible projects look effortless. When I’m not wrangling AWS services or having fun building cool things, I’m either jamming on my guitar, diving into historical rabbit holes, or perfecting the art of sarcastic wit.",
+    your_img_url: "https://soundbytez.s3.us-east-2.amazonaws.com/IMG_9461+2.jpg",
+};
+
+const dataabout = {
+    title: "who even is this guy?",
+    aboutme: "I bring order to cloud chaos, making tech work smarter, not harder. Clouds may float aimlessly in the sky, but not on my watch. Whether it’s scaling workloads, crafting SoWs that could win a Pulitzer (if they gave awards for that), or brainstorming outside-the-box solutions, I do it all with a mix of strategy, precision, and a good sense of humor.",
+};
+const worktimeline = [{
+        jobtitle: "Technical Account Manager",
+        where: "AWS",
+        date: "2025",
+    },
+    {
+        jobtitle: "Solutions Architect",
+        where: "Cloud303 - An AWS Premier Partner",
+        date: "2022-2025",
+    },
+    {
+        jobtitle: "Media Mogul",
+        where: "Mainstream Media",
+        date: "2005-2022",
+    },
+];
+
+const skills = [{
+        name: "AWS",
+        value: 100,
+    },
+    {
+        name: "GenAI",
+        value: 85,
+    },
+    {
+        name: "Containers, Serverless, CICD",
+        value: 100,
+    },
+    {
+        name: "Security & Networking",
+        value: 80,
+    },
+    {
+        name: "Customer Obsession (tbh, I'm just a yapper)",
+        value: 100,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jonathan.m.fernandez@gmail.com",
+    YOUR_FONE: "(904)-616-5383",
+    description: "Let’s connect! Whether you have a project idea, a collaboration opportunity, or just want to chat about tech, feel free to drop a message. I’m always open to exploring new possibilities and making great things happen. Reach out—I’d love to hear from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com/in/jonfernz",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
